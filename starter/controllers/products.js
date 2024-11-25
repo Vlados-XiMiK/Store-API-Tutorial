76dee2307359e2f5aea63149a8e0613f0d2aab3c
unknown <tvladislav0504@gmail.com>
2024-11-25 17:23:40 +0200

    139 refactor to queryObject

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -7,7 +7,16 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
+  const { featured } = req.query
+
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  console.log(queryObject)
+
+  const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
 
