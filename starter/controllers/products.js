83542bacab9e980a6318971afb7971c33f4ceecd
unknown <tvladislav0504@gmail.com>
2024-11-28 13:31:38 +0200

    146 pagination

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,11 +1,8 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({})
-    .sort('name')
-    .select('name price')
-    .limit(10)
-    .skip(1)
+  const products = await Product.find({}).sort('name').select('name price')
+
   res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
@@ -38,6 +35,14 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
+  // 23
+  // 4 7 7 7 2
 
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
